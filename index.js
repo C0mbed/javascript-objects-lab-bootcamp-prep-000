085a2b65b5397182152aa6ee5442572c93668b5b
Matt Farncombe <matt@farncombe.co>
2020-04-17 01:47:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function updateObjectWithKeyAndValue(object, key, value){
-  console.log('key and value is ' + key + ' ' + value);
   var result = Object.assign({}, object);
   result[key] = value;
   return result;
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+
 }
